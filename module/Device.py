5d7f6a67c39d8e093b2dc624ad8a2542cb713c99
bejaouij <bejaoui.je@mail.com>
2019-01-21 16:43:05 +0100

    feat($lib): create method to retrieve device data
    Add a method to retrieve a specific device data.

--- module/Device.py
+++ module/Device.py
@@ -1,13 +1,26 @@
+from module import database
+
 class Device:
 	DEVICE_STATE_INACTIVE = 0
 	DEVICE_STATE_ACTIVE = 1
-	DEVICE_STATE_SUSPECT_ACTIVITY = 2
-	DEVICE_STATE_STOLE = 3
+	VEHICLE_STATE_SUSPECT_ACTIVITY = 3
+	VEHICLE_STATE_STOLE = 4
+	VEHICLE_STATE_OK = 5
 
-	serialNumber = ""
-	state = 0
+	serialNumber = None
+	device_status = None
+	vehicle_status = None
 	inDisabling = False
 	triggeredTheftProcess = False
 
 	def __init__(self, serialNumber):
 		self.serialNumber = serialNumber
+
+	def checkState(self):
+		data = database.retrieveDeviceData(self.serialNumber)
+		self.device_status = data['status_code']
+		self.vehicle_status = data['vehicle_status']
+		self.inDisabling = data['in_disabling']
+
+		if self.vehicle_status == self.VEHICLE_STATE_STOLE:
+			self.triggeredTheftProcess = True
